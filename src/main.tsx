b09555d739c38c32135c99a3f2b1ef652019164b
Asur0w <lolplayers000@gmail.com>
2026-01-25 10:57:30 +0100

    Sans bug?

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css' // <--- CETTE LIGNE EST CRUCIALE POUR LE DESIGN
+import App from './App'
+import './index.css' // C'est cette ligne qui est importante !
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
