700aa6bd6672b385bd6d1e45bb0766c8bc04ad2f
Asur0w <lolplayers000@gmail.com>
2026-01-25 09:47:33 +0100

    V150

--- src/App.tsx
+++ src/App.tsx
@@ -1149,7 +1149,7 @@ export default function App() {
             from { opacity: 0; transform: scale(0.95); }
             to { opacity: 1; transform: scale(1); }
         }
-
+        
         .animate-fade-in { animation: zoomIn 0.7s ease-out forwards; }
         .animate-fade-in-up { animation: fadeInUp 1s ease-out forwards; }
         .animate-fade-in-right { animation: fadeInRight 0.5s ease-out forwards; }
